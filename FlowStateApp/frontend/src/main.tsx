88c33c6717a311cfece8cdd6cfc33a288f9fde97
V_R <viswasreddy1804@gmail.com>
2026-05-18 17:32:47 +0530

    modified:   FlowStateApp/frontend/src/main.tsx 	modified:   FlowStateApp/frontend/vite.config.ts

--- FlowStateApp/frontend/src/main.tsx
+++ FlowStateApp/frontend/src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/flowstate'>
       <App />
     </BrowserRouter>
   </StrictMode>,
